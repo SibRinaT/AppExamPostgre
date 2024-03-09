d2f8cafdd9b0bcb072b43086f9036f327495c2da
Ainur <sibrinat616@gmail.com>
2024-03-09 20:56:47 +0300

    trying connect to PostgreSQL

---
+++ AppExamPostgre/AppExamPostgre/AppExamPostgreApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppExamPostgreApp.swift
+//  AppExamPostgre
+//
+//  Created by Ainur on 07.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppExamPostgreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppExamPostgre/AppExamPostgre/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  AppExamPostgre
+//
+//  Created by Ainur on 07.03.2024.
+//
+
+import Foundation
